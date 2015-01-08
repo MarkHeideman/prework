e9c0e05d646068116cea7aed8d281a54822f65da
Mark Heideman <cptdpft@erthlink.net>
2015-01-08 15:00:11 -0700

    Started the Prework. Took forever to debug first homework assignment. Hope we are supposed to use same "prework" file? ch5 ch6 all in same file ch7 but not read, just typed in code.

--- index.php
+++ index.php
@@ -220,9 +220,172 @@ if ($display_prices) {
 }
 ?>
 
+<?php
+$num = -321;
+$newnum = abs($num);
+echo $newnum;
+//prints "321"
+?>
+
+<?php
+function bighello()
+{
+     echo "<h1>HELLO!</h1>";
+}
+     bighello();
+	?>
 
 
 
+
+<$php
+function printBR($txt)
+{
+	echo $txt."<br/>";
+}
+printBR("This is a line.");
+printBR("This is a new line.");
+printBR("This is yet abother line.");
+?>
+
+
+<$php
+function addNums($firstnum, $secondnum)
+{
+		$result = $firstnum + $secondnum;
+		return $result;
+}
+echo addNums(3,5);
+//will print "8"
+?>
+
+<$php
+function test()
+{
+		$testvariable = "this is a test variable";
+	}
+echo "test variable: ".$testvariable."<br/>";
+?>
+
+
+<$php
+$life = 42;
+function meaningOfLife()
+{
+		echo "The meaning of life is ".$life";
+}
+meanOfLife();
+?>
+
+
+
+
+<$php
+$life=42;
+	function meaningOfLife()
+{
+		global $life;
+		echo "The meaning of life is ".$life";
+}
+	meaningOfLife();
+	?>
+
+
+<$php
+$num_0f_calls = 0;
+	function numberedHeading($txt)
+{
+			global $num_of_calls:
+	$num_of_calls++;
+	echo "<h1>".$num_of_calls." ".$txt."</h1>";
+}
+	echo "<p>We build a fine range of widgets.</p>';
+numberedHeading("Doodads");
+	echo "<p>Finest in the world.</p.;
+		?>
+
+
+
+<$php
+function numberedHeading($txt)
+{
+	static $num_of_calls = 0;
+	$num_of_calls++;
+	echo "<h1>".$num_of_calls." ".$txt."</h1>";
+}
+numberedHeading("Widgets");echo
+echo "<p>We build a fine range of widgets.</p>";
+numberedHeading("Doodads");
+echo "<p>Finest in the world.</p>";
+?>
+
+
+<$php
+	function fontwrap($txt, $fontsize)
+{
+	echo "<span style="\"font-size:$fontsize\">.$txt."</span?";
+}
+	fontWrap("A heading<br/>","24pt");
+	fontWrap("some body text<br/>","12pt");
+	fontWrap("smaller body text<br/>","10pt");
+?>
+
+
+<$php
+function fontWrap($txt, $fontsize = "12pt")
+{
+	echo "<span style="\"font-size:$fontsize\">".$txt."</span>";
+}
+fontWrap("A Heading<br/>", "24pt");
+fontWrap("some body text<br/>");
+fontWrap("even smaller body text<br/>";
+?>
+
+
+
+
+<$php
+ function addFive($num)
+{
+		$num +=5;
+}
+$orignum = 10;
+addFive($orignum);
+echo $orignum;
+?>
+
+
+<?php
+function addFive(&$num)
+{
+		$num +=5;
+}
+$orignum = 10;
+addFive($orignum);
+echo $orignum;
+?>
+
+
+<$php
+function tagWrap($tag, $txt, $func = "")
+{
+		if ((!empty($txt)) && (function_exist($func))) {
+			$txt + $func($txt);
+			return "<".$tag.">'.$txt."</".$tag."><bt/>";
+		} else {
+			return "<strong>".$txt."</strong><br/>";
+		}
+}
+
+function underline($txt)
+{
+	return "<span style=\"text-decoration:underline;\">".$txt."</span>";
+}
+echo tagWrasp('strong', 'make me bold');
+echo tagWrap('em', 'underline and italicize me', "underline");
+echo tagWrap('em' 'make me italic and quote me',
+create_function('$txt', 'return "&quot;$txt&quot;";'));
+?>
 
 
 
