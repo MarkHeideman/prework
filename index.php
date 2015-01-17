040670be290680ad57fddaab371ad6012756e370
Mark Heideman <cptdpft@erthlink.net>
2015-01-17 16:34:35 -0700

    One week into class, five days..I'm ready to quit. Starting over with this being a three day weekend(from class). Going to see if I can catch up so material starts making sense or am I a lost cause. Maybe not a lost cause as maybe a slow learner. Maybe need a tutor and ALOT MORE TIME to learn material? It's Saturday, late noon...almost 5pm. Where did the time go?

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
 		<body>
 			<p>I completed the Prework</p>
 			<p>All I've done is follow the screencast and typed this info in<br></p>
-			<p>I've read two and a half of the material, but have not done any of the book exercises</p>
+			<p>I've read two and a half of the books of the homework material, <br>but have not done any of the book exercises</p>
 			<p>Now as a <em>'start over</em> I need to see if I stay in class, quit, or transfer into the next available class<br></em></p>
+			<p>Saturday of three day weekend, I've started re-reading HTML5 and CSS3<br> and I'm @ Appendix C. First half I understood.</p>
+			<p>Maybe I need a plan D, Enroll in next available class,</p>
+			<p>and hire Ryan as a weekly Tutor to help me out, check on progress, and give me weekly goals to achieve.</p>
 		</body>
 	</html>
 
@@ -24,3 +27,16 @@
 
 		</body>
 	</html>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Inline script</title>
+	</head>
+	<body>
+		<script>
+			window.alert("Inline script!");
+		</script>
+	</body>
+</html>
