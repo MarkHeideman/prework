c45294df7e430960cba7bc060d3a68fbfee769c0
Mark Heideman <cptdpft@erthlink.net>
2015-01-16 18:44:39 -0700

    One week into class, five days..I'm ready to quit. Starting over with this being a three day weekend(from class). Going to see if I can catch up so material starts making sense or am I a lost cause.

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 			<title>I Completed the Prework</title>
 		</head>
 		<body>
-			</p>I completed the Prework<p>
+			<p>I completed the Prework</p>
+			<p>All I've done is follow the screencast and typed this info in<br></p>
+			<p>I've read two and a half of the material, but have not done any of the book exercises</p>
+			<p>Now as a <em>'start over</em> I need to see if I stay in class, quit, or transfer into the next available class<br></em></p>
 		</body>
 	</html>
 
